fcbe80562389b4701346f49a4fb5f3bf0c165926
KemalAbdic <kemal.abdic2@gmail.com>
2021-11-02 23:23:11 +0100

    - removed unnecessary annotation

--- src/main/java/com/atlantbh/auctionapp/security/WebSecurityConfig.java
+++ src/main/java/com/atlantbh/auctionapp/security/WebSecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
