9d6a0db02877c215d9876504eb24ebe91941f736
Kemal Abdić <53895313+KemalAbdic@users.noreply.github.com>
2021-12-05 21:54:13 +0100

    - added grid / list switch to shop page (#11)

--- frontend/src/pages/ShopPage/index.js
+++ frontend/src/pages/ShopPage/index.js
@@ -12,6 +12,8 @@ const Shop = ({match}) => {
     const history = useHistory();
     const {removeBreadcrumb} = useBreadcrumbContext();
     const [maxRange, setMaxRange] = useState(9);
+    const [viewStyle, setViewStyle] = useState("grid");
+    const [activeButton, setActiveButton] = useState(0);
     const url = match.url.split("/").slice(2, -1);
 
     useEffect(() => {
@@ -35,6 +37,15 @@ const Shop = ({match}) => {
         setMaxRange(prevMaxRange => prevMaxRange + 9)
     }
 
+    const handleGridChange = (e) => {
+        setActiveButton(0)
+        setViewStyle(e);
+    }
+    const handleListChange = (e) => {
+        setActiveButton(1)
+        setViewStyle(e);
+    }
+
     return (<div className="shop-page-wrapper">
         <div className="categories-shop-container">
             <ListGroup variant="flush">
@@ -45,36 +56,78 @@ const Shop = ({match}) => {
             </ListGroup>
         </div>
         <div className="shop-product-container">
-            <div className="shop-sorting-bar">
-                <Form.Select>
-                    <option value="1">Default Sorting</option>
-                    <option value="2">Sort by Popularity</option>
-                    <option value="3">Sort by Rating</option>
-                    <option value="4">Sort by Newness</option>
-                    <option value="5">Sort by Price</option>
-                </Form.Select>
-                <div className="arrow-icon"><Icon icon="akar-icons:chevron-down" inline={true}/></div>
-
+            <div className="sorting-grid-list">
+                <div className="shop-sorting-bar">
+                    <Form.Select>
+                        <option value="1">Default Sorting</option>
+                        <option value="2">Sort by Popularity</option>
+                        <option value="3">Sort by Rating</option>
+                        <option value="4">Sort by Newness</option>
+                        <option value="5">Sort by Price</option>
+                    </Form.Select>
+                    <div className="arrow-icon"><Icon icon="akar-icons:chevron-down"/></div>
+                </div>
+                <div className="toggle-style">
+                    <Button style={activeButton === 0 ? {color: '#8367d8'} : null}
+                            aria-pressed={activeButton ? '0' : '1'} className="toggle-button"
+                            onClick={() => handleGridChange("grid")}>
+                        <Icon icon="mdi-light:grid" width="24" height="24" inline={true}/><span className="toggle-text"
+                                                                                                style={{paddingRight: '27px'}}>Grid</span></Button>
+                    <Button style={activeButton !== 0 ? {color: '#8367d8'} : null}
+                            aria-pressed={activeButton ? '0' : '1'} className="toggle-button"
+                            onClick={() => handleListChange("list")}>
+                        <Icon icon="mdi-light:menu" width="24" height="24" inline={true}/><span
+                        className="toggle-text">List</span></Button>
+                </div>
             </div>
-            <div className="shop-product-grid">
-                {categoryProducts
+            {viewStyle === "grid" ?
+                <div className="shop-grid">
+                    {categoryProducts
+                        .filter((product, i) => i < maxRange)
+                        .map(product => (
+                            <div className="shop-product"
+                                 onClick={() =>
+                                     history
+                                         .push(`/shop/${product.categoryName
+                                             .toLowerCase()}/${product.subcategoryName
+                                             .toLowerCase()}/${product.id}`)}>
+                                <div className="shop-product-item"><Image width="262"
+                                                                          height="196"
+                                                                          src={product.url}
+                                />
+                                    <h5 className="shop-product-title">{product.name}</h5>
+                                    <span className="shop-product-price">Start From
+                                <span style={{color: '#8367D8'}}> ${product.startingPrice}</span></span></div>
+                            </div>))}
+                </div>
+                :
+                <div className="shop-list">{categoryProducts
                     .filter((product, i) => i < maxRange)
                     .map(product => (
-                        <div className="shop-product"
+                        <div className="shop-list-product"
                              onClick={() =>
                                  history
                                      .push(`/shop/${product.categoryName
                                          .toLowerCase()}/${product.subcategoryName
                                          .toLowerCase()}/${product.id}`)}>
-                            <div className="shop-product-item"><Image width="262"
-                                                                      height="196"
-                                                                      src={product.url}
-                            />
-                                <h5 className="shop-product-title">{product.name}</h5>
-                                <span className="shop-product-price">Start From
-                                <span style={{color: '#8367D8'}}> ${product.startingPrice}</span></span></div>
+                            <div className="shop-list-item">
+                                <Image width="306" height="230" src={product.url}/>
+                                <div className="shop-list-info">
+                                    <h5 className="shop-list-title">{product.name}</h5>
+                                    <span className="shop-list-description"> {product.description}</span>
+                                    <span className="shop-list-price">Start From
+                                <span style={{color: '#8367D8'}}> ${product.startingPrice}</span>
+                                </span>
+                                    <Button className="shop-list-bid-button"><span
+                                        className="shop-list-bid-button-text">Bid</span>
+                                        <Icon icon="ic:outline-gavel" color="#c4c4c4" width="24" height="24"
+                                              hFlip={true} inline={true}/>
+                                    </Button>
+                                </div>
+                            </div>
                         </div>))}
-            </div>
+                </div>}
+
             {categoryProducts.length > maxRange ? <div className="explore-more-div">
                 <Button className="explore-more-button" onClick={handleClick}>EXPLORE MORE </Button>
             </div> : null}
