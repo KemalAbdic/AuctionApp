d03ea462a9f3dc454688684c54b44d3a1f72f1c6
KemalAbdic <kemal.abdic2@gmail.com>
2021-12-30 11:29:37 +0100

    - price filter working on shop page, category and subcategory page  - reworked categories generation from frontend to backend  - categories open when going to shop page from breadcrumbs or from landing page categories list  - subcategories now show number of products in them  - shop page products now have bid button and transparent overlay on hover  - written tests

--- frontend/src/common/PriceFilter/index.js
+++ frontend/src/common/PriceFilter/index.js
@@ -1,37 +1,52 @@
 import React, {useEffect, useState} from "react";
 
 import './priceFilter.css';
-
-import RangeSlider from 'rsuite/RangeSlider';
 import "rsuite/dist/rsuite.min.css";
+import {Slider} from "@material-ui/core";
 
-
-const PriceFilter = ({minPrice, maxPrice}) => {
-    const [avg, setAvg] = useState();
-
+const PriceFilter = ({priceRange, setPriceRange, handleClick}) => {
+    const [minPrice, setMinPrice] = useState(0);
+    const [maxPrice, setMaxPrice] = useState(400);
+    const [avgPrice, setAvgPrice] = useState(0);
 
     useEffect(() => {
-        setAvg(((minPrice + maxPrice) / 2).toFixed(2))
-    }, [minPrice, maxPrice])
+        setPriceRange({
+            min: 0,
+            max: 400
+        });
+        setAvgPrice(((priceRange.min + priceRange.max) / 2).toFixed(2));
+        // eslint-disable-next-line
+        }, [setMinPrice, setMaxPrice])
+
+    useEffect(() => {
+        setAvgPrice(((priceRange.min + priceRange.max) / 2).toFixed(2));
+    }, [priceRange])
+
+    const onSliderChange = (e, newValues) => {
+        setPriceRange({
+            min: newValues[0],
+            max: newValues[1]
+        });
+        handleClick({minPrice: priceRange.min, maxPrice: priceRange.max})
+    }
 
     return (
         <div className="price-filter-container">
             <p className="price-range-filter-title"> Price Range</p>
-            <div className="input-box-wrapper">
-                <input className="first-input-box" placeholder={minPrice}/>
-                <input className="second-input-box" placeholder={maxPrice}/>
-            </div>
-
-            <RangeSlider min={minPrice} max={maxPrice}/>
-
+            <Slider
+                min={minPrice}
+                max={maxPrice}
+                value={[priceRange.min, priceRange.max]}
+                step={1}
+                onChange={onSliderChange}
+            />
             <div className="price-range">
-                {"$" + minPrice + "-$" + maxPrice}
+                {"$" + priceRange.min + "-$" + priceRange.max}
             </div>
             <div className="price-range-average">
-                The average price is ${avg}
+                The average price is ${avgPrice}
             </div>
         </div>
-
     )
 }
 
